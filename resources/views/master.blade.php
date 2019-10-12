3ca24cf9d1f64323d28c8049fc091a63f8ed9f78
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-10-12 13:27:41 -0300

    master blade adicionado

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,23 +10,23 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <li class="nav-item {!! request()->is('/') ? 'active':'' !!} ">
+            <a class="nav-link" href="/">Home  </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact</a>
+        <li class="nav-item {!! request()->is('contact') ? 'active':'' !!} ">
+            <a class="nav-link " href="/contact"> Contact</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {!! request()->is('about') ? 'active':'' !!} ">
             <a class="nav-link" href="/about">About Us</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {!! request()->is('tickets') ? 'active':'' !!} ">
             <a class="nav-link" href="/tickets">Tickets</a>
         </li>
         </ul>
